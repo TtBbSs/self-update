4fda40b92ccfed119a5516e4133232cd34ebdfe8
TtBbSs <bagelillekage@gmail.com>
2021-01-08 12:10:03 +0100

    1.1.1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # self-update
+git tag <version 1.1.0>
+git push origin <version 1.1.0>
---
+++ self-update.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ *
+ * Plugin Name: a self-update
+ * Description: TBS.
+ * Version: 1.1.2
+ * Author: TBS
+ * WC requires at least: 3.3
+ * WC tested up to: 4.8
+ *
+ */
+?>
